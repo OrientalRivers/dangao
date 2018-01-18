f75497d4c9c4e337c0240e5a89d934a4d8039ad0
username <565368208@qq.comm>
2018-01-18 15:23:48 +0800

    项目初步建立

---
+++ pages/index/index.js
@@ -0,0 +1,20 @@
+//获取应用实例
+var app = getApp()
+Page({
+    data: {
+        // indicatorDots: true,
+        // vertical: false,
+        // autoplay: true,
+        // interval: 3000,
+        // duration: 1000,
+        // loadingHidden: false  // loading
+    },
+
+    //事件处理函数
+    swiperchange: function(e) {
+        //console.log(e.detail.current)
+    },
+
+    onLoad: function() {
+    }
+})
